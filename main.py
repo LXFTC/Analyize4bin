1abd8f2f579004b3ef62f66dba84cef56edca1e5
LXFTC <3277169280@qq.com>
2024-04-09 17:11:34 +0800

    主函数，实现了函数调用，以及绘制亮度和id图层的功能

---
+++ main.py
@@ -0,0 +1,39 @@
+import cv2
+import tifffile
+import matplotlib.pyplot as plt
+import numpy as np
+import os 
+from PIL import Image
+import split_circle
+
+ 
+Wimg = cv2.imread('sobel_tiff\\0513173604_BL1_10.tiff', cv2.IMREAD_ANYDEPTH)
+Bimg = cv2.imread('sobel_tiff\\0513173810_FAM1_11.tiff', cv2.IMREAD_ANYDEPTH)
+Gimg = cv2.imread('sobel_tiff\\0513174113_HEX1_12.tiff', cv2.IMREAD_ANYDEPTH)
+
+print(f"Wimg读进来的格式:{Wimg.dtype}+{Wimg.shape}")
+print(f"Bimg读进来的格式:{Bimg.dtype}+{Bimg.shape}")
+print(f"Gimg读进来的格式:{Gimg.dtype}+{Gimg.shape}")
+
+# 图像增强
+Wimg = split_circle.enhance_contrast(Wimg, 10, 3000)
+Bimg = split_circle.enhance_contrast(Bimg, 50, 4000)#30，4000
+Gimg = split_circle.enhance_contrast(Gimg, 80, 3000)
+
+print(f"Wimg转换后的格式:{Wimg.dtype}+{Wimg.shape}")
+print(f"Bimg转换后的格式:{Bimg.dtype}+{Bimg.shape}")
+print(f"Gimg转换后的格式:{Gimg.dtype}+{Gimg.shape}")
+
+# cv2.imshow("Bimg",Bimg)
+# cv2.imshow("Gimg",Gimg)
+# cv2.imshow("Wimg",Wimg)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+#对Wimg做sobel算子边缘检测
+split_circle.sobel_edge_detection(Wimg)
+split_circle.connected_component_process("sobel_edge_image.tiff")
+split_circle.draw_mask_circle2BG (Bimg)
+split_circle.draw_mask_circle2BG (Gimg)
+# 调用函数
+split_circle.draw_pixel_at_circle_center(Bimg)
