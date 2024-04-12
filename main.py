9b92125c94522e5496257580450acdbce789c851
LXFTC <3277169280@qq.com>
2024-04-12 13:39:56 +0800

    fix:
    修改了json文件生成时的bug，目前可以正常生成blue.json和green.json，以及之前主函数调用时的bug，由于调用了两次draw_mask_circle2BG（）函数，导致存放圆心信息的全局列表global_circle_list 叠加了两次，所以blue.json 内的对象重复了一次

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import numpy as np
 import os 
 from PIL import Image
 import split_circle
-from threading import Thread, Lock
+import threading 
  
-Wimg = cv2.imread('sobel_tiff\\0513173604_BL1_10.tiff', cv2.IMREAD_ANYDEPTH)
-Bimg = cv2.imread('sobel_tiff\\0513173810_FAM1_11.tiff', cv2.IMREAD_ANYDEPTH)
-Gimg = cv2.imread('sobel_tiff\\0513174113_HEX1_12.tiff', cv2.IMREAD_ANYDEPTH)
+Wimg = cv2.imread('sobel_tiff\\0513170420_BL1_07.tiff', cv2.IMREAD_ANYDEPTH)
+Bimg = cv2.imread('sobel_tiff\\0513170602_FAM1_08.tiff', cv2.IMREAD_ANYDEPTH)
+Gimg = cv2.imread('sobel_tiff\\0513170900_HEX1_09.tiff', cv2.IMREAD_ANYDEPTH)
 
 print(f"Wimg读进来的格式:{Wimg.dtype}+{Wimg.shape}")
 print(f"Bimg读进来的格式:{Bimg.dtype}+{Bimg.shape}")
@@ -28,25 +28,42 @@ print(f"Gimg转换后的格式:{Gimg.dtype}+{Gimg.shape}")
 #对Wimg做sobel算子边缘检测
 split_circle.sobel_edge_detection(Wimg)
 split_circle.connected_component_process("sobel_edge_image.tiff")
+# lock = threading.Lock()
 
-def thread_Func(img,color_of_light):
-    split_circle.draw_mask_circle2BG (img)
-    split_circle.draw_pixel_at_circle_center(img,color_of_light)
-    split_circle.generate_json_file(color_of_light)
+# def thread_Func(img,color_of_light):
+#     lock.acquire()
+#     split_circle.draw_mask_circle2BG (img)
+#     split_circle.draw_pixel_at_circle_center(img,color_of_light)
+#     split_circle.generate_json_file(color_of_light)
+#     lock.release()
+    
 
 
-Thread1 = Thread(target=thread_Func, args=(Bimg,'blue'))
-# Thread2 = Thread(target=thread_Func, args=(Gimg,'green'))
+# Thread1 = threading.Thread(target=thread_Func, args=(Bimg,'blue'))
+# Thread1.start()
+# Thread1.join() 
 
-Thread1.start()    
+# Thread2 = threading.Thread(target=thread_Func, args=(Gimg,'green'))
 # Thread2.start()
+# Thread2.join()    
 
-Thread1.join()    
-# Thread2.join()
+split_circle.draw_mask_circle2BG (Gimg)
+split_circle.draw_pixel_at_circle_center(Gimg,'green')   
+split_circle.generate_json_file('green')
+
+
+split_circle.draw_pixel_at_circle_center(Bimg,'blue')   
+split_circle.generate_json_file('blue')
+
+# split_circle.merge_json('通道1.json')
 
 
 
 
+# split_circle.generate_json_file('blue')
+# split_circle.generate_json_file('green')
+
+# split_circle.merge_json('blue.json','green.json')
 
 
 
--- split_circle.py
+++ split_circle.py
@@ -14,7 +14,6 @@ global_blue_list = []
 #定义一个全局列表，用于保存绿光下液滴的信息，包括圆心位置，半径，亮度值
 global_green_list = []
 
-
 #图像增亮
 def enhance_contrast(img, alpha, beta):
     # 提高对比度
@@ -27,6 +26,7 @@ def enhance_contrast(img, alpha, beta):
     img = np.clip(img, 0, 255)
     return img
 
+
 #开运算函数
 def opening(img):
     original_image = img.copy()
@@ -36,6 +36,7 @@ def opening(img):
                                [-1, -1, -1]])
     opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel_opening, 3)#去毛刺
     cv2.imwrite("opening.tiff",opening)
+
 
 #sobel算子边缘检测
 def sobel_edge_detection(img):
@@ -75,14 +76,15 @@ def connected_component_process(img):
                    [-1,  8, -1],
                    [-1, -1, -1]])
     opening_mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-    # 显示掩码
-    cv2.imshow("openging_Mask", opening_mask)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # # 显示掩码
+    # cv2.imshow("openging_Mask", opening_mask)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
     cv2.imwrite("mask.tiff",mask)
     cv2.imwrite("opening_mask.tiff",opening_mask)
     print(f"mask:{mask.dtype}_{mask.shape}")
+
 
 #根据sobel算子得到的mask做圆形检测，并将检测到的圆心以及半径存储到全局列表
 def draw_mask_circle2BG (img):
@@ -101,13 +103,6 @@ def draw_mask_circle2BG (img):
                    [-1, -1, -1]])
     img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
 
-    # enhance_contrast(img, 1.5, 0)
-    # adjusted_img = cv2.addWeighted(img, 100, np.zeros(img.shape, img.dtype), 50, 0) #这个函数是可用的
-    #增强图像亮度
-    # 定义亮度增强参数
-    # brightness_factor = 254
-    # adjusted_img = np.where((255 - img) < brightness_factor, 255, img * 100) #这个函数是可用的
-
     #在Bimg或Gimg上绘制圆
     if circles is not None:
         circles = np.uint16(np.around(circles))
@@ -117,8 +112,9 @@ def draw_mask_circle2BG (img):
             global_circle_list.append((i[0], i[1],i[2]))
             # print(f"圆心坐标：{i[0]},{i[1]}")
 
+
 #在输入图像上绘制亮度文本以及圆形
-def draw_pixel_at_circle_center(img,color_of_light):
+def draw_pixel_at_circle_center(img,color_light):
     height, width = img.shape
     bgr_image = img.copy()
     bgr_image = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
@@ -127,10 +123,10 @@ def draw_pixel_at_circle_center(img,color_of_light):
         if 0 <= center[0] < width and 0 <= center[1] < height:
             # 获取圆心处的像素值
             px_value = img[center[1], center[0]]
-        if color_of_light == "blue":
+        if color_light == "blue":
             # 将圆心处的像素值保存到全局列表
             global_blue_list.append((center[0], center[1], center[2], int (px_value*10-200)))
-        elif color_of_light == "green":
+        elif color_light == "green":
             # 将圆心处的像素值保存到全局列表
             global_green_list.append((center[0], center[1], center[2], int (px_value*10-200)))
         # 在圆心处绘制像素值
@@ -142,19 +138,21 @@ def draw_pixel_at_circle_center(img,color_of_light):
         # 在圆心处绘制圆
         cv2.circle(bgr_image, (center[0], center[1]), center[2], (0, 0, 255), 1)
     
-    cv2.imwrite(f"img_with_pixel_values_at_circle_centers_{color_of_light}.tiff",bgr_image)
+    cv2.imwrite(f"img_with_pixel_values_at_circle_centers_{color_light}.tiff",bgr_image)
+    print (f"global_blue_list:{global_blue_list}")
+    print(f"global_green_list:{global_green_list}")
 
+    
 #根据global_blue_list和global_green_list生成json文件
 def generate_json_file(color_of_light):
-    json_file_name = "通道1.json"
-    json_data = {}
-    json_data["circles"] = []
     if color_of_light == "blue":
-        data_list = global_blue_list
-        for data in data_list:
+        json_file_name = "blue.json"
+        json_data = {}
+        json_data["circles"] = []
+        for data in global_blue_list:
             # 定义json文件中每个数据项的内容
             data_content = {"id":f"{data[0]}_{data[1]}", "x": data[0], "y": data[1], "radius": data[2], "FAM": data[3],"islive":"true"}
-            # 将uint16类型的数据转换为int类型
+         # 将uint16类型的数据转换为int类型
             if isinstance(data[0], np.uint16):
                 data_content["x"] = int(data[0])
             if isinstance(data[1], np.uint16):
@@ -162,16 +160,27 @@ def generate_json_file(color_of_light):
             if isinstance(data[2], np.uint16):
                 data_content["radius"] = int(data[2])
             if isinstance(data[3], np.uint16):
-                data_content["brightness"] = int(data[3])
+                data_content["FAM"] = int(data[3])
             
             # 将数据项内容添加到json文件中
             json_data["circles"].append(data_content)  # 这里应该使用json_data["circles"]而不是circles
                 
         json_data["lightid"] = "1"
+        # 保存json文件
+        try:
+            with open(json_file_name, "w") as f:
+                    
+                f.write(json.dumps(json_data, indent=4))
+                print(f"已保存{color_of_light}光下液滴的json文件：{json_file_name}")
+        except PermissionError:
+            print("没有足够的权限来创建或写入文件")  
+
 
     if color_of_light == "green":
-        data_list = global_green_list
-        for data in data_list:
+        json_file_name = "green.json"
+        json_data = {}
+        json_data["circles"] = []
+        for data in global_green_list:
             # 定义json文件中每个数据项的内容
             data_content = {"id":f"{data[0]}_{data[1]}", "x": data[0], "y": data[1], "radius": data[2], "HEX": data[3],"islive":"true"}
             # 将uint16类型的数据转换为int类型
@@ -182,24 +191,27 @@ def generate_json_file(color_of_light):
             if isinstance(data[2], np.uint16):
                 data_content["radius"] = int(data[2])
             if isinstance(data[3], np.uint16):
-                data_content["brightness"] = int(data[3])
-            
+                data_content["HEX"] = int(data[3])
+                
             # 将数据项内容添加到json文件中
             json_data["circles"].append(data_content)  # 这里应该使用json_data["circles"]而不是circles
-                
+                    
         json_data["lightid"] = "1"
-
-
         # 保存json文件
         try:
             with open(json_file_name, "w") as f:
-                
+                    
                 f.write(json.dumps(json_data, indent=4))
                 print(f"已保存{color_of_light}光下液滴的json文件：{json_file_name}")
         except PermissionError:
-            print("没有足够的权限来创建或写入文件")        
-            
+            print("没有足够的权限来创建或写入文件")  
+
+
+     
+
+
     
+        
    
     
 
